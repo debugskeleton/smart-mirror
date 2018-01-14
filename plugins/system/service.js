09eac0813a7e564dcaaea9e305a2f93f054ef915
debugskeleton <debugskeleton@gmail.com>
2018-01-14 10:58:12 -0500

    testing system info module

---
+++ plugins/system/service.js
@@ -0,0 +1,41 @@
+(function () {
+	'use strict';
+
+	function SystemService() {
+		var service = {};
+        const si = require('systeminformation');
+
+        // callback style
+        si.cpu(function(data) {
+            console.log('CPU-Information:');
+            console.log(data);
+        });
+
+        // promises style - new in version 3
+        si.cpu()
+            .then(data => console.log(data))
+            .catch(error => console.error(error));
+
+        // full async / await example (node >= 7.6)
+        async function cpu() {
+          try {
+            const data = await si.cpu();
+            console.log(data)
+          } catch (e) {
+            console.log(e)
+          }
+        }
+    }
+
+    service.init = function (cb) {
+
+
+
+    }
+
+    return service;
+
+	angular.module('SmartMirror')
+    .factory('SystemService', SystemService);
+
+} ());
