3d946a282728f0b0ffcf24daf2d7f0a5d4de7757
debugskeleton <debugskeleton@gmail.com>
2018-01-14 11:18:29 -0500

    test

--- plugins/system/service.js
+++ plugins/system/service.js
@@ -4,32 +4,32 @@
 	function SystemInfoService() {
 		var service = {};
         const si = require('systeminformation');
-    }
 
-    service.init = function (cb) {
-        // callback style
-        si.cpu(function(data) {
-            console.log('CPU-Information:');
-            console.log(data);
-        });
+        service.init = function (cb) {
+            // callback style
+            si.cpu(function(data) {
+                console.log('CPU-Information:');
+                console.log(data);
+            });
 
-        // promises style - new in version 3
-        si.cpu()
-            .then(data => console.log(data))
-            .catch(error => console.error(error));
+            // promises style - new in version 3
+            si.cpu()
+                .then(data => console.log(data))
+                .catch(error => console.error(error));
 
-        // full async / await example (node >= 7.6)
-        async function cpu() {
-          try {
-            const data = await si.cpu();
-            console.log(data)
-          } catch (e) {
-            console.log(e)
-          }
+            // full async / await example (node >= 7.6)
+            async function cpu() {
+              try {
+                const data = await si.cpu();
+                console.log(data)
+              } catch (e) {
+                console.log(e)
+              }
+            }
         }
-    }
 
-    return service;
+        return service;
+    }
 
 	angular.module('SmartMirror')
     .factory('SystemInfoService', SystemInfoService);
